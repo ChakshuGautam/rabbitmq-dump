bce9264f3917f2298e4ccb1e277db1a0d1272b5c
chaks03 <chakshu@samagragovernance.in>
2021-07-06 07:50:10 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var jackrabbit = require("jackrabbit");
+const fs = require("fs");
+var url =
+  process.env.AMQP_URL || "amqp://username:password@139.59.46.189:5672/";
+var topic = process.env.TOPIC || "celery";
+
+console.log("Opening connection to RabbitMQ");
+
+var rabbit = jackrabbit(url);
+var exchange = rabbit.default();
+
+const msgQueue = exchange.queue({ name: topic, durable: true });
+
+msgQueue.consume(sendMsg, { noAck: true });
+
+counter = 0;
+function sendMsg(data) {
+  counter++;
+  process.stdout.write("\033[0G");
+  process.stdout.write(`${counter}/11000000`);
+  fs.appendFileSync("messages.txt", `${data[0]}\n`);
+}
